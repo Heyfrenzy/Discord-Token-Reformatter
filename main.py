d78991837d33ead025b4956d64bf6c67823a5ae9
Pixens <107793019+Pixens@users.noreply.github.com>
2022-11-24 22:24:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def get_all_tokens(filename:str): #returns all tokens in a file as token from email:password:token
+    all_tokens = []
+    for j in open(filename, "r").read().splitlines():
+        if ":" in j:
+            j = j.split(":")[2]
+            all_tokens.append(j)
+        else:
+            all_tokens.append(j)
+ 
+    return all_tokens
+
+all_tokens = get_all_tokens("tokens.txt")
+open("tokens.txt", "w").write("".join(all_tokens))
